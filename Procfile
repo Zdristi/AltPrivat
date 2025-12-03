c14fa7ea685572ec9257d6da498d85f55cce8736
Zdristi <pelakhindriy@gmail.com>
2025-12-03 17:07:53 +0200

    Remove gunicorn dependency and revert to simple HTTP server

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn --bind 0.0.0.0:$PORT app:app
+web: python simple_telegram_bot.py
--- app.py
+++
@@ -1,5 +0,0 @@
-# app.py - точка входа для Render
-from simple_telegram_bot import application
-
-# Render ожидает объект 'app' в файле 'app.py'
-app = application
